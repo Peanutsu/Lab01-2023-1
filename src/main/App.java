bf94098a30ebe1df6121dc247b7ab9a9672d0ccf
Peanutsu <118844854+Peanutsu@users.noreply.github.com>
2023-10-19 20:31:18 +0000

    primeiro teste

--- src/main/App.java
+++ src/main/App.java
@@ -1,11 +1,28 @@
 package main;
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        int a = 0; 
+        for(int i = 0; i < arrayOrdenado.length; i++){
+            if (arrayOrdenado[i] == num) {
+                a=num;
+            }
+        }
+        if (a == num){
+            return 1;
+        }
+        else{
+            return 0;
+        }
     }
 
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        int a = arrayDesordenado[0];
+        for (int i = 0; i < arrayDesordenado.length; i++){
+            if (a < arrayDesordenado[i]){
+                a = arrayDesordenado[i];
+            }
+        }
+        return a;
     }
 
     public static Integer parMaisProximo(Integer[][] pares){
